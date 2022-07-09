bdca24ee1fd0cdc62bfbeb1ba2f3bfdb27365b66
Edward Ji <jiziao6@gmail.com>
2022-07-09 23:15:52 +1000

    Fix adc instruction SREG

--- instruction.py
+++ instruction.py
@@ -2,8 +2,14 @@ from register import Register
 
 
 def adc(machine, d, r):
-    machine.R[d].val = machine.R[d].val + machine.R[r].val + machine.SREG.C
-    machine.SREG.C = 0
-    if not 0 <= machine.R[d].val < Register.MAX_VAL:
-        machine.R[d].val %= Register.MAX_VAL
-        machine.SREG.C = 1
+    Rd, Rr = machine.R[d], machine.R[r]
+    SREG = machine.SREG
+
+    R = Register(None, val=Rd.val + Rr.val + SREG.C)
+    SREG.H = Rd[3] and Rr[3] or Rr[3] and not R[3] and not R[3] and Rd[3]
+    SREG.V = Rd[7] and Rr[7] and not R[7] or not Rd[7] and not Rr[7] and R[7]
+    SREG.S = SREG.N ^ SREG.V
+    SREG.N = R[7]
+    SREG.Z = not R
+    SREG.H = Rd[7] and Rr[7] or Rr[7] and not R[7] or not R[7] and Rd[7]
+    Rd.val = R.val
