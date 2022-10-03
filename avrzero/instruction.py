3fb28cbf9c4c79a7144323934e5da5a1652e72b0
Edward Ji <jiziao6@gmail.com>
2022-10-04 03:18:23 +1100

    Fix typo in ADD instruction
    It still has the "plus carry bit" from the copying and pasting of ADC.

--- avrzero/instruction.py
+++ avrzero/instruction.py
@@ -384,7 +384,7 @@ def add(machine, d, r):
     Rd, Rr = machine.R[d], machine.R[r]
     R = SREG = machine.SREG
 
-    val = Rd.val + Rr.val + SREG.C
+    val = Rd.val + Rr.val
     SREG.H = Rd[3] and Rr[3] or Rr[3] and not R[3] and not R[3] and Rd[3]
     SREG.V = Rd[7] and Rr[7] and not R[7] or not Rd[7] and not Rr[7] and R[7]
     SREG.S = SREG.N != SREG.V
