30275b478ccf9eb1bcb246823e3ad1b50168ff1e
Edward Ji <jiziao6@gmail.com>
2022-10-04 13:54:13 +1100

    Add hello world example (#6)

---
+++ avrzero/example/hello_world.asm
@@ -0,0 +1,11 @@
+; load the ASCII values of helloworld to R16:25
+ldi R16, 104
+ldi R17, 101
+ldi R18, 108
+ldi R19, 108
+ldi R20, 111
+ldi R21, 119
+ldi R22, 111
+ldi R23, 114
+ldi R24, 108
+ldi R25, 100
